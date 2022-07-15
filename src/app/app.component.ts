18712a7cc9bea0cd03dd4c7ca96ff008bb1f29b8
Md. Musta-E-Nur Rahman <md.musta-e-nur.rahman@b-architects.com>
2022-07-15 09:04:33 +0900

    Card showing site finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountain lover',
+      content: 'I saw this neat mountain today'
+    },
+    {
+      title: 'Bicycle',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'bike lover',
+      content: 'I saw this neat bike today'
+    },
+    {
+      title: 'Bicycle1',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'bike lover1',
+      content: 'I saw this neat bike1 today'
+    }
+  ]
 }
